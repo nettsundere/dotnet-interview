5852c1055008c186c08248bafb044e9ece57b5e5
Vladimir nettsundere Kiselev <nettsundere@gmail.com>
2020-02-17 04:48:52 +0300

    Add async example

---
+++ Simple/Async/App.cs
@@ -0,0 +1,22 @@
+﻿using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Async
+{
+    public class App
+    {
+        private static readonly HttpClient Client = new HttpClient();
+        
+        private const string SomeSite = "https://example.com";
+        
+        public Task<HttpResponseMessage> ComputeAsync()
+        {
+            return Client.GetAsync(SomeSite);
+        }
+
+        public async Task<HttpResponseMessage> AwaitComputeAsync()
+        {
+            return await Client.GetAsync(SomeSite).ConfigureAwait(false);
+        }
+    }
+}
---
+++ Simple/Runner/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Runner
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var asyncApp = new Async.App();
+
+            var resultWithoutAwait = await asyncApp.ComputeAsync();
+            Console.WriteLine(resultWithoutAwait.StatusCode);
+
+            var resultWithAwait = await asyncApp.AwaitComputeAsync();
+            Console.WriteLine(resultWithAwait.StatusCode);
+        }
+    }
+}
